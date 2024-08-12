5ca571961dee6b49325bf49706d832632a148975
HiMark1 <91948952+HiMark1@users.noreply.github.com>
2024-08-13 00:29:26 +0300

    Add files via upload

---
+++ hhAutoUpResum/HHAutoUpResum/HHAutoUpResum/Services/BrowserFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PuppeteerSharp;
+
+namespace TestProject01.Services
+{
+    internal class BrowserFactory : IBrowserFactory
+    {
+        private readonly LaunchOptions _options;
+        // Указываем путь к существующему профилю Браузера
+        //string userProfilePath = @"C:\Users\msave\AppData\Local\Google\Chrome\User Data";
+        //string browserPath = @"C:\Program Files\Google\Chrome\Application\chrome.exe";
+        public BrowserFactory(string userProfilePath, string browserPath)
+        {
+            _options = new LaunchOptions
+            {
+                Headless = true, // Устанавливаем false, чтобы видеть браузер
+                ExecutablePath = browserPath,
+                UserDataDir = userProfilePath
+            };
+        }
+
+        public async Task<IBrowser> GetBrowserAsync()
+        {
+
+            return await Puppeteer.LaunchAsync(_options);
+        }
+
+        public async Task<IPage> GetPageAsync(IBrowser browser)
+        {
+            return await browser.NewPageAsync();
+        }
+    }
+}
