b9339f08b232cdb5b70722d8bdbd55ce2470e765
HiMark1 <91948952+HiMark1@users.noreply.github.com>
2024-08-16 03:42:30 +0300

    Update 01

---
+++ HHAutoUpResum/HHAutoUpResum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using PuppeteerSharp;
+using TestProject01.Models;
+using TestProject01.Services;
+using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        IConfiguration Configuration = new ConfigurationBuilder()
+       .SetBasePath(Directory.GetCurrentDirectory())
+       .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+       .Build();
+
+        var userProfilePath = Configuration["BrowserSettings:UserProfilePath"];
+        var browserPath = Configuration["BrowserSettings:BrowserPath"];
+        if(browserPath == null || userProfilePath == null) { Console.WriteLine("Ошибка! Не найдены browserPath или userProfilePath"); }
+        // Настройка DI
+        var serviceProvider = new ServiceCollection()
+            .AddSingleton<IBrowserFactory, BrowserFactory>(sp => new BrowserFactory(userProfilePath, browserPath))
+            .AddSingleton<IAuthService, AuthService>()
+            .AddSingleton<IResumeService, ResumeService>()
+            .BuildServiceProvider();
+
+
+        var browserFactory = serviceProvider.GetService<IBrowserFactory>();
+        var authService = serviceProvider.GetService<IAuthService>();
+        var resumeService = serviceProvider.GetService<IResumeService>();
+
+
+
+        var browser = await browserFactory.GetBrowserAsync();
+        var page = await browserFactory.GetPageAsync(browser);
+
+        await resumeService.RaiseResumeAsync(page);
+
+        await browser.CloseAsync();
+    }
+}
---
+++ HHAutoUpResum/HHAutoUpResum/Services/AuthService.cs
@@ -0,0 +1,23 @@
+﻿using PuppeteerSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject01.Models;
+
+namespace TestProject01.Services
+{
+    internal class AuthService : IAuthService
+    {
+        public async Task LoginAsync(Page page, UserCredentials credentials)
+        {
+            await page.GoToAsync("https://hh.ru/account/login");
+            await page.TypeAsync("input[type='text' i]", credentials.Username);
+            await page.TypeAsync("#password", credentials.Password);
+            await page.ClickAsync(".magritte-button_size-large___Gjw7e_5-0-8");
+            await page.WaitForNavigationAsync();
+        }
+    }
+}
+    
